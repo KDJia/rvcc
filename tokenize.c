e5a6ab86a3963541b7c329ddbd52953ee96534f1
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-08-24 15:47:37 +0800

    [53] 支持->操作符

--- tokenize.c
+++ tokenize.c
@@ -153,10 +153,14 @@ static int fromHex(char C) {
 
 // 读取操作符
 static int readPunct(char *Ptr) {
-  // 判断2字节的操作符
-  if (startsWith(Ptr, "==") || startsWith(Ptr, "!=") || startsWith(Ptr, "<=") ||
-      startsWith(Ptr, ">="))
-    return 2;
+  // 判断多字节的操作符
+  static char *KW[] = {"==", "!=", "<=", ">=", "->"};
+
+  // 遍历列表匹配Ptr字符串
+  for (int I = 0; I < sizeof(KW) / sizeof(*KW); ++I) {
+    if (startsWith(Ptr, KW[I]))
+      return strlen(KW[I]);
+  }
 
   // 判断1字节的操作符
   return ispunct(*Ptr) ? 1 : 0;
